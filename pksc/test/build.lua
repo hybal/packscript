2bcef76e84204d5cf2c87a85d5eddf759264d8da
ellipse12 <ellipse1234567@gmail.com>
2025-01-20 18:26:21 -0700

    added metadata to workspace

--- pksc/test/build.lua
+++ pksc/test/build.lua
@@ -4,7 +4,7 @@ function download(name)
         local data = fetch("https://github.com/ellipse12/Pack/archive/refs/heads/main.zip")
         write(name..".zip", data)
 end
-for i = 1,5 do
+for i = 1,20 do
         download(tostring(i))
 end
 
