05d6df83f5bf027079b8ba845de5a0c06460a63e
ellipse12 <ellipse1234567@gmail.com>
2025-02-18 14:29:05 -0700

    regex functions now use fancy_regex crate and are no longer unstable

--- core/src/builtin/regex.rs
+++ core/src/builtin/regex.rs
@@ -1,39 +1,35 @@
 //! Regex functions. 
-//! > **Warning:** The regex API is current unstable and is subject to change
 use mlua::prelude::*;
 use pksc_macros::*;
 use crate::*;
-use onig::*;
-//TODO: make it find every match instead of just the first one on each line
+use fancy_regex::Regex;
+
+
+
+///takes a string and a pattern and returns an array with all of the matches for that pattern. 
 fn rmatch(lua: &Lua, (src, patt): (String, String)) -> LuaResult<mlua::Value> {
     let regex = Regex::new(&patt).map_err(|err| mlua::Error::RuntimeError(format!("Could not compile regex pattern: {}", err)))?;
-    let mut out: Vec<mlua::Table> = vec![];
-    for line in src.lines() {
-        if let Some(captures) = regex.captures(line) {
-            let matches: Vec<String> = captures
-                .iter()
-                .flatten()
-                .map(|m| m.to_string())
-                .collect();
-            out.push(lua.create_sequence_from(matches)?);
-        }    
-    }
-    Ok(mlua::Value::Table(lua.create_sequence_from(out.iter())?))
+    let matches: Result<Vec<_>, _> = regex.captures_iter(&src)
+        .map(|caps| caps.unwrap().iter().flatten()
+            .map(|val| lua.create_string(val.as_str()))
+            .collect::<Result<Vec<_>, _>>())
+        .map(|res| res.and_then(|value| lua.create_sequence_from(value.iter())))
+        .collect();
+    let matches = matches?;
+    Ok(mlua::Value::Table(lua.create_sequence_from(matches.iter())?))
 }
 
-//TODO: support all types of backreferences
+///replaces all occurences of `patt` in `src` using `rep`
+fn replace_all(_lua: &Lua, (src, patt, rep): (String, String, String)) -> LuaResult<String> {
+    let regex = Regex::new(&patt).map_err(|err| mlua::Error::RuntimeError(format!("Could not compile regex pattern: {}", err)))?;
+    let replaced = regex.replace_all(&src, &rep).to_string();
+    Ok(replaced)
+}
+
+///replaces the first occurence of `patt` in `src` using `rep`
 fn replace(_lua: &Lua, (src, patt, rep): (String, String, String)) -> LuaResult<String> {
     let regex = Regex::new(&patt).map_err(|err| mlua::Error::RuntimeError(format!("Could not compile regex pattern: {}", err)))?;
-    let replaced = regex.replace_all(&src, |caps: &Captures| {
-        let mut out = rep.clone();
-        for (i, group) in caps.iter().enumerate() {
-            if let Some(group_str) = group {
-                out = out.replace(&format!(r"\{}", i), group_str);
-                
-            }
-        }
-        out
-    }).to_string();
+    let replaced = regex.replace(&src, &rep).to_string();
     Ok(replaced)
 }
 
@@ -43,7 +39,8 @@ pub fn register(lua: &Lua) -> LuaResult<()> {
     let re = lua.create_table()?;
     set_table_functions!(lua, re,
         "matches" => rmatch,
-        "replace" => replace,
+        "replace_all" => replace_all,
+        "replace" => replace
     );
     set_globals!(lua,
         "re" => re,
